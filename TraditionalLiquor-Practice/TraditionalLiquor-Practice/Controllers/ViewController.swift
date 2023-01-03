b136cece8e1f58f44a4a8c15ee92bbb2613ae6a6
yeojinyun <sail041@naver.com>
2023-01-03 23:16:30 +0900

    cell 구성

--- TraditionalLiquor-Practice/TraditionalLiquor-Practice/Controllers/ViewController.swift
+++ TraditionalLiquor-Practice/TraditionalLiquor-Practice/Controllers/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 class ViewController: UIViewController {
     
     var liquorData: [TraditionalLiquor]?
+    private let tableView: UITableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         getPath()
-        
+        setLayout()
+        setBasics()
     }
 
 
@@ -30,4 +32,45 @@ class ViewController: UIViewController {
             liquorData = liquor
         }
     }
+}
+
+// MARK: -UI
+extension ViewController {
+    final private func setLayout() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.frame = view.frame
+    }
+    
+    final private func setBasics() {
+        //MARK: -Delegate, Data Source
+        tableView.register(LiquorTableViewCell.self.self, forCellReuseIdentifier: LiquorTableViewCell.identifier)
+        tableView.rowHeight = 230
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    private func setConstraints() {
+        
+    }
+    
+}
+
+//MARK: -Delegate, Data Source
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: LiquorTableViewCell.identifier, for: indexPath) as? UITableViewCell else { return UITableViewCell() }
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
 }
---
+++ TraditionalLiquor-Practice/TraditionalLiquor-Practice/Views/LiquorTableViewCell.swift
@@ -0,0 +1,73 @@
+//
+//  LiguorTableViewCell.swift
+//  TraditionalLiquor-Practice
+//
+//  Created by 순진이 on 2023/01/03.
+//
+
+import UIKit
+
+class LiquorTableViewCell: UITableViewCell {
+    
+    static let identifier = String(describing: LiquorTableViewCell.self)
+    
+    // 이름, 도수, 용량, 주재료, 제조사
+    private let title: UILabel = UILabel()
+    private let alcohol: UILabel = UILabel()
+    private let volume: UILabel = UILabel()
+    private let mainIngredient: UILabel = UILabel()
+    private let manufacturer: UILabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUI()
+        setConstraints()
+        setTest()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
+
+extension LiquorTableViewCell {
+    private func setUI() {
+        [title, alcohol, volume, mainIngredient, manufacturer].forEach {
+            contentView.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            title.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+            title.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            
+            alcohol.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 20),
+            alcohol.leadingAnchor.constraint(equalTo: title.leadingAnchor),
+            
+            volume.topAnchor.constraint(equalTo: alcohol.bottomAnchor, constant: 20),
+            volume.leadingAnchor.constraint(equalTo: title.leadingAnchor),
+            
+            mainIngredient.topAnchor.constraint(equalTo: volume.bottomAnchor, constant: 20),
+            mainIngredient.leadingAnchor.constraint(equalTo: title.leadingAnchor),
+            
+            manufacturer.topAnchor.constraint(equalTo: mainIngredient.bottomAnchor, constant: 20),
+            manufacturer.leadingAnchor.constraint(equalTo: title.leadingAnchor),
+        ])
+    }
+    
+    private func setTest() {
+        title.text = "애피소드호프"
+        alcohol.text = "3.5"
+        volume.text = "500ml"
+        mainIngredient.text = "사과과즙, 보리, 쌀, 홉"
+        manufacturer.text = "㈜한국애플리즈"
+    }
+}
