0410f15533e98a379f5be0a9a72cae0778746625
yeojinyun <sail041@naver.com>
2023-01-31 23:48:33 +0900

    button configure

--- TraditionalLiquor-Practice/TraditionalLiquor-Practice/Controllers/RecordViewController.swift
+++ TraditionalLiquor-Practice/TraditionalLiquor-Practice/Controllers/RecordViewController.swift
@@ -40,5 +40,19 @@ extension RecordViewController {
 }
 
 extension UIButton.Configuration {
-    
+    static func setWineButtonStyle(_ title: String, image: UIImage) -> UIButton.Configuration {
+        var configuration = self.plain()
+        var titleAttributed = AttributedString.init(title)
+        titleAttributed.font = UIFont(name: "GowunBatang-Regular", size: 18)
+        titleAttributed.foregroundColor = .darkGray
+        configuration.title = title
+        configuration.titleAlignment = .center
+        configuration.image = image
+//        configuration.baseForegroundColor = .darkGray
+//        configuration.baseBackgroundColor = .myGreen
+        configuration.imagePlacement = .top
+        configuration.imagePadding = 8
+        configuration.attributedTitle = titleAttributed
+        return configuration
+    }
 }
